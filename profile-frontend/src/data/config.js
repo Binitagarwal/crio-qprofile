60cc47b2f4fd7b291196500a900eefa2054867eb
Binit Agarwal <rockingbinitagarwal@gmail.com>
2021-01-21 13:20:00 +0530

    My first commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Binit Agarwal",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Binit Agarwal and I'm a Software Engineer!",
+  githubUrl: "https://github.com/Binitagarwal",
+  linkedinUrl: "https://www.linkedin.com/in/binit-agarwal-447184178/",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
